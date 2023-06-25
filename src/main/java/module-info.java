b3264e4d5a8c910b5892c9c7125478dad3a3273a
Troy Pilewski <12568877+troypilewski@users.noreply.github.com>
2023-06-24 17:18:37 -0700

    Added Main User Interface; Added Nitrite Dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.app.nitritevisualizer {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires nitrite;
 
     opens com.app.nitritevisualizer to javafx.fxml;
     exports com.app.nitritevisualizer;
